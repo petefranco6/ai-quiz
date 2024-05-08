1a72470099829e817d805be93aee10a4fd5fc469
Pete Franco <petethe4th@live.com>
2024-05-08 01:37:28 -0500

    nav, auth

---
+++ src/components/Providers.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+import { ThemeProvider as NextThemeProvider, ThemeProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
+
+const Providers = ({ children, ...props }: ThemeProviderProps) => {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <SessionProvider>{children}</SessionProvider>
+    </ThemeProvider>
+  );
+};
+
+export default Providers;
